f440f225aa5ad1df1a58f486480771132c964c92
Mapenzi Moono <123325927+Moono25@users.noreply.github.com>
2026-04-24 02:22:15 +0200

    Create DataWarehouse database with schemas

---
+++ Scripts/Create_Database.sql
@@ -0,0 +1,17 @@
+USE master;
+
+--Create a new database named DataWarehouse
+CREATE DATABASE DataWarehouse;
+
+-- Switch to the DataWarehouse database
+
+USE DataWarehouse;
+
+-- Create three schemas named Bronze, Silver, and Gold
+
+CREATE SCHEMA Bronze;
+GO
+CREATE SCHEMA Silver;
+GO
+CREATE SCHEMA Gold;
+GO
